fd2d9475a75da2e3afd18e3af53cc8ce5d44607c
yuli-r <iuliana.rotaru234@gmail.com>
2023-10-13 17:44:10 +0100

    Add recipe update controller

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable consistent-return */
@@ -156,10 +157,97 @@ exports.recipe_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display recipe update form on GET.
 exports.recipe_update_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Recipe update GET');
+  const [recipe, allAuthors, allCategories] = await Promise.all([
+    Recipe.findById(req.params.id).populate('author').populate('category').exec(),
+    Author.find().exec(),
+    Category.find().exec(),
+  ]);
+
+  if (recipe === null) {
+    const err = new Error('Recipe not found!');
+    err.status = 404;
+    return next(err);
+  }
+
+  for (const category of allCategories) {
+    for (const recipe_cat of recipe.category) {
+      if (category._id.toString() === recipe_cat._id.toString()) {
+        category.checked = 'true';
+      }
+    }
+  }
+
+  res.render('recipe_form', {
+    title: 'Update recipe',
+    authors: allAuthors,
+    categories: allCategories,
+    link_to_recipe: recipe.link_to_recipe,
+    recipe,
+  });
 });
 
 // Handle recipe update on POST.
-exports.recipe_update_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Recipe update POST');
-});
+exports.recipe_update_post = [
+  (req, res, next) => {
+    if (!(req.body.category instanceof Array)) {
+      if (typeof req.body.category === 'undefined') {
+        req.body.category = [];
+      } else {
+        req.body.category = new Array(req.body.category);
+      }
+    }
+    next();
+  },
+
+  body('title', 'Title must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('author', 'Author must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('description', 'Description must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('category.*').escape(),
+  body('link_to_recipe', 'Link to recipe must not be empty').trim().isLength({ min: 1 }),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const recipe = new Recipe({
+      title: req.body.title,
+      author: req.body.author,
+      description: req.body.description,
+      category: typeof req.body.category === 'undefined' ? [] : req.body.category,
+      link_to_recipe: req.body.link_to_recipe,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      const [allAuthors, allCategories] = await Promise.all([
+        Author.find().exec(),
+        Category.find().exec(),
+      ]);
+
+      for (const category of allCategories) {
+        if (recipe.category.indexOf(category._id) > -1) {
+          category.checked = 'true';
+        }
+      }
+
+      res.render('recipe_form', {
+        title: 'Update recipe',
+        authors: allAuthors,
+        categories: allCategories,
+        recipe,
+        errors: errors.array(),
+      });
+    } else {
+      const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, recipe, {});
+      res.redirect(updatedRecipe.url);
+    }
+  }),
+];
