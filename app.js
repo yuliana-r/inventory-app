b41d6da5bd674fa7f9db899e9a7e30cbf00ebf26
yuli-r <iuliana.rotaru234@gmail.com>
2023-10-24 21:19:56 +0100

    Remove redundant files

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 /* eslint-disable no-use-before-define */
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog');
+const indexRouter = require("./routes/index");
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
 dotenv.config();
 const mongoDB = `${process.env.MONGODB_URI}`;
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -23,17 +22,17 @@ async function main() {
 }
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/catalog', catalogRouter);
+app.use("/", indexRouter);
+app.use("/catalog", catalogRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -45,11 +44,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
