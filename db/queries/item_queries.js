335c93abcc2b6c7d7add372ebcb75140372ae0b7
Yuliana R <iuliana.rotaru234@gmail.com>
2025-07-03 20:02:22 +0100

    Amend getAllItems db query to include left join

--- db/queries/item_queries.js
+++ db/queries/item_queries.js
@@ -1,7 +1,20 @@
 const pool = require('../pool');
 
 async function getAllItems() {
-  const { rows } = await pool.query('SELECT * FROM items');
+  const { rows } = await pool.query(`
+  SELECT 
+    items.item_id,
+    items.name AS item_name,
+    items.qty,
+    units.name AS unit_name,
+    categories.name AS category_name,
+    brands.name AS brand_name
+  FROM items
+  LEFT JOIN units ON items.unit_id = units.unit_id
+  LEFT JOIN categories ON items.category_id = categories.category_id
+  LEFT JOIN brands ON items.brand_id = brands.brand_id
+`);
+
   return rows;
 }
 
