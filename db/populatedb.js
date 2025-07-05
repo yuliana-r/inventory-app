2c7249e0a9e55b2348cab4f7528e58547a47f8d8
Yuliana R <iuliana.rotaru234@gmail.com>
2025-07-05 22:56:09 +0100

    Add user messages for instances where the database has no records in it

--- db/populatedb.js
+++ db/populatedb.js
@@ -28,26 +28,55 @@ CREATE TABLE IF NOT EXISTS items (
   FOREIGN KEY (brand_id) REFERENCES brands (brand_id),
   FOREIGN KEY (unit_id) REFERENCES units (unit_id)
 );
-
-INSERT INTO categories (name)
-VALUES
-('Cupboard'), ('Fridge'), ('Freezer');
-
-INSERT INTO brands (name)
-VALUES
-('Sainsbury''s'), ('Tesco'), ('M & S'), ('Aldi');
-
-INSERT INTO units (name)
-VALUES
-('g'), ('piece'), ('kg'), ('bag'), ('ml');
-
-INSERT INTO items (name, qty, unit_id, category_id, brand_id)
-VALUES
-('flour', 700, 1, 1, 4),
-('prawns', 1, 4, 3, 1),
-('apples', 10, 2, 2, 2),
-('potatoes', 2.5, 3, 1, 3);
 `;
+
+// const SQL = `
+// CREATE TABLE IF NOT EXISTS categories (
+//   category_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//   name VARCHAR (20) NOT NULL
+// );
+
+// CREATE TABLE IF NOT EXISTS brands (
+//   brand_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//   name VARCHAR (20) NOT NULL
+// );
+
+// CREATE TABLE IF NOT EXISTS units (
+//   unit_id SERIAL PRIMARY KEY,
+//   name VARCHAR (20) NOT NULL UNIQUE
+// );
+
+// CREATE TABLE IF NOT EXISTS items (
+//   item_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//   name VARCHAR (20) NOT NULL,
+//   qty NUMERIC(10,2),
+//   unit_id INTEGER,
+//   category_id INTEGER,
+//   brand_id INTEGER,
+//   FOREIGN KEY (category_id) REFERENCES categories (category_id),
+//   FOREIGN KEY (brand_id) REFERENCES brands (brand_id),
+//   FOREIGN KEY (unit_id) REFERENCES units (unit_id)
+// );
+
+// INSERT INTO categories (name)
+// VALUES
+// ('Cupboard'), ('Fridge'), ('Freezer');
+
+// INSERT INTO brands (name)
+// VALUES
+// ('Sainsbury''s'), ('Tesco'), ('M & S'), ('Aldi');
+
+// INSERT INTO units (name)
+// VALUES
+// ('g'), ('piece'), ('kg'), ('bag'), ('ml'), ('l');
+
+// INSERT INTO items (name, qty, unit_id, category_id, brand_id)
+// VALUES
+// ('flour', 700, 1, 1, 4),
+// ('prawns', 1, 4, 3, 1),
+// ('apples', 10, 2, 2, 2),
+// ('potatoes', 2.5, 3, 1, 3);
+// `;
 
 async function main() {
   console.log('seeding...');
